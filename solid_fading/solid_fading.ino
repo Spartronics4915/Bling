992dab8fd86d0bed0a6eb404b46a7dc8003e5a9f
unknown <adamrr100@gmail.com>
2018-02-14 18:08:46 -0800

    Fades in and out, switching between two colors (blue and yellow)

--- solid_fading/solid_fading.ino
+++ solid_fading/solid_fading.ino
@@ -1,9 +1,11 @@
 #include "FastLED.h"
 
 
+#define NUM_COLORS 2
 #define NUM_LEDS 8
 #define DATA_PIN 11
 #define DELAY_TIME 6
+
 
 
 #define FADING_COLOR1 CRGB(0, 50, 170)  //blue
@@ -11,25 +13,41 @@
 
 CRGB leds[NUM_LEDS];  
 
+bool blue = false;
+
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   for (int i=0 ; i < NUM_LEDS; i++) {
-        leds[i] = FADING_COLOR1;
+        leds[i] = FADING_COLOR2;
   }
   FastLED.setBrightness(30);
 }
 
 void loop() {
-  for(int brightness = 20; brightness<=155; brightness++){
+  for(int brightness = 3; brightness<=155; brightness++){
     FastLED.setBrightness(brightness);
     FastLED.show();
     delay(DELAY_TIME);  
   }
-  for(int brightness = 155; brightness>=20; brightness--){
+  
+  for(int brightness = 155; brightness>=3; brightness--){
     FastLED.setBrightness(brightness);
     FastLED.show();
     delay(DELAY_TIME);  
   }
+  if (blue){
+    for (int i=0 ; i < NUM_LEDS; i++) {
+          leds[i] = FADING_COLOR2;
+          blue = false;
+    }
+  } else {
+    for (int i=0 ; i < NUM_LEDS; i++) {
+          leds[i] = FADING_COLOR1;
+          blue = true; 
+    }
+  }
+  FastLED.show();
+
 }
 
 void displayColor(CRGB color) {
