f39d89fdf3c5caef9547e8df97661acbab75f826
unknown <adamrr100@gmail.com>
2018-02-24 11:47:11 -0800

    Roborio functionality with 4 colors and 4 effects

--- Roborio_functionality/Roborio_functionality/Roborio_functionality.ino
+++ Roborio_functionality/Roborio_functionality/Roborio_functionality.ino
@@ -1,11 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 
-#define BLACK Adafruit_NeoPixel::Color(10,10,10)//actually a very dim white
-#define BLUE Adafruit_NeoPixel::Color(0, 0, 255)
-#define RED Adafruit_NeoPixel::Color(255, 0, 0)
-#define YELLOW Adafruit_NeoPixel::Color(255, 255, 0)
-#define GREEN Adafruit_NeoPixel::Color(0, 255, 0)
+#define BLACK Adafruit_NeoPixel::Color(0,0,0)//actually a dim white
+#define BLUE Adafruit_NeoPixel::Color(0, 0, 128)
+#define RED Adafruit_NeoPixel::Color(128, 0, 0)
+#define YELLOW Adafruit_NeoPixel::Color(128, 128, 0)
+#define GREEN Adafruit_NeoPixel::Color(0, 128, 0)
 #define PURPLE Adafruit_NeoPixel::Color(128,0,128)
+
 
 #define NUM_LEDS 150
 #define PIN 11
@@ -14,15 +15,18 @@ enum {
   DEFAULT_LEDS,
   BLUE_LEDS,
   YELLOW_LEDS,
-  GREEN_LEDS,
+  GREEN_LEDS, 
   RED_LEDS,
   PURPLE_LEDS,
 };
+
 
 int blingMode = DEFAULT_LEDS;
 bool flashing = false;
 bool fast_flashing = false;
 bool fading = false;
+bool spartronics_fade = false;
+bool blue = true;
 
 // allocate our pixel memory, configuYELLOW to match our hardware
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB+NEO_KHZ800);
@@ -40,6 +44,7 @@ void setup() {
   Serial.begin(9600);
   pixels.begin(); // initialize neopixel library
   clearStrip(BLACK);
+  Serial.println("Hello LEDS!");
 }
 void loop() {
   
@@ -48,10 +53,13 @@ void loop() {
   while (Serial.available() > 0)
   {
     messageByte = Serial.read();
-    Serial.println("loop " +messageByte);
+    //Serial.println("loop " +messageByte);
     //each time gets new input, set most recent blingMode    
     switch (messageByte)
     {
+      case 'a':
+      blingMode = PURPLE_LEDS;
+      break;
       case '0':
         blingMode = DEFAULT_LEDS;
         break;
@@ -68,7 +76,7 @@ void loop() {
       blingMode = GREEN_LEDS;
       break;
       case '5':
-      blingMode = PURPLE_LEDS;
+      spartronics_fade = true;
       break;
       case '6':
       fading = true;
@@ -77,18 +85,23 @@ void loop() {
       fast_flashing = true;
       break;
       case'8':
+      //resets all effects
       flashing = false;
       fast_flashing = false;
       fading = false;
-      pixels.setBrightness(255);
+      spartronics_fade = false;
+      pixels.setBrightness(100);
       break;
       case '9':
       flashing = true;
       break;
+      /*case 'd':
+      theaterChase();
+      break;*/
       /* Add more modes here */
       //default:
-        // The byte was not for a valid message, so we ignore it...
-      //break;
+      //break;        // The byte was not for a valid message, so we ignore it...
+
     }
   }
   //keep updating LED strip
@@ -101,7 +114,6 @@ void loop() {
       case BLUE_LEDS:
         clearStrip(BLUE);
         //Serial.println("Blue");
-
         break;
       case YELLOW_LEDS:
         clearStrip(YELLOW);
@@ -131,22 +143,56 @@ void loop() {
         delay(150);
           }
    if(fading){
-  for(int brightness = 3; brightness<=255; brightness++){
+  for(int brightness = 3; brightness<=100; brightness++){
     pixels.setBrightness(brightness);
     pixels.show();
-    delay(0.5);  
+    delay(10);  
   }
-  
-  for(int brightness = 255; brightness>=3; brightness--){
+  for(int brightness = 100; brightness>=3; brightness--){
     pixels.setBrightness(brightness);
     pixels.show();
-    delay(0.5);  
+    delay(10);
    
-          
-
+      }
+   }
+   if(spartronics_fade){
+    
+          if (blue){
+          clearStrip(BLUE);
+          blue = false;
+  } else {
+          clearStrip(YELLOW);
+          blue = true; 
+      }
+      for(int brightness = 3; brightness<=100; brightness++){
+    pixels.setBrightness(brightness);  
+    pixels.show();
+    delay(10);  
+  }
+  for(int brightness = 100; brightness>=3; brightness--){
+    pixels.setBrightness(brightness);
+    pixels.show();
+    delay(10);
+   
       }
    }
 }
- /* end of loop func */
+/* end of loop func */
 
 
+/*void theaterChase(uint8_t wait) {
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < pixel_stick.numPixels(); i=i+3) {
+        pixel_stick.setPixelColor(i+q, Wheel( (i) % 255));    //turn every third pixel on
+      }
+       pixel_stick.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < pixel_stick.numPixels(); i=i+3) {
+        pixel_stick.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}*/
+ 
